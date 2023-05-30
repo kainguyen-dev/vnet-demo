9305bbea7f9d29228ae53ebe52377aa6cc1a25e0
Duyntc <congduy@us.maxnerva.com>
2023-05-30 09:16:26 +0700

    ADD ui

---
+++ ui/src/app/app.component.ts
@@ -0,0 +1,94 @@
+//src\app\app.component.ts
+import { Component, OnInit } from '@angular/core';
+import { WebsocketService } from "./websocket-service.service";
+
+
+@Component({
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"],
+  providers: [WebsocketService]
+})
+
+export class AppComponent implements OnInit {
+
+  counterProduct: number = 15;
+  counterStore: number = 15;
+
+
+  public data: any = {
+    "product": {},
+    "store": {},
+  }
+  public displayProduct: any[] = [];
+  public displayStore: any[] = [];
+
+  public setting: Object = {
+    aggregate: "byProduct",
+    ws: "aggragate-by-product"
+  }
+
+  constructor(
+    private productWebsocketService: WebsocketService,
+    private storeWebsocketService: WebsocketService // Add a second instance variable
+  ) { }
+
+
+  ngOnInit(): void {
+
+    setInterval(() => {
+      this.counterProduct--;
+      this.counterStore--;
+    }, 1000);
+
+    this.productWebsocketService.connect('ws://localhost:8080/aggragate-by-product');
+
+    this.productWebsocketService.getMessages().subscribe((message) => {
+
+    
+      if (message["group"] === "/aggragate-by-product") {
+        this.counterProduct = 15;
+
+        this.data["product"][message["entity"]] = message["data"];
+
+        this.displayProduct = Object.keys(this.data["product"]).map(key => {
+          const store = JSON.parse(this.data["product"][key]);
+          return {
+            name: key,
+            SalesUnits: store.SalesUnits,
+            SalesRevenue: store.SalesRevenue
+          };
+        });
+      }
+    });
+
+
+    this.storeWebsocketService.connect('ws://localhost:8080/aggragate-by-store');
+
+    this.storeWebsocketService.getMessages().subscribe((message) => {
+
+      if (message["group"] === "/aggragate-by-store") {
+
+        this.counterStore = 15;
+
+        this.data["store"][message["entity"]] = message["data"];
+
+        this.displayStore = Object.keys(this.data["store"]).map(key => {
+          const store = JSON.parse(this.data["store"][key]);
+          return {
+            name: key,
+            SalesUnits: store.SalesUnits,
+            SalesRevenue: store.SalesRevenue
+          };
+        });
+      }
+    });
+
+  }
+
+  ngOnDestroy(): void {
+    this.productWebsocketService.close();
+    this.storeWebsocketService.close();
+  }
+
+}
